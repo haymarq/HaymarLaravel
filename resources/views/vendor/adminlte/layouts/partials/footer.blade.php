31cfd4f3c2a71aad75c20e1157ff6fdc61cf297a
BryantJimenez <bryant.luis1600186@gmail.com>
2018-11-07 23:57:02 -0400

    Iniciando proyecto en Laravel 5.4

---
+++ resources/views/vendor/adminlte/layouts/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+        <b>Proyecto realizado por Haymar Quintero.</b>
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2018.</strong>
+</footer>
